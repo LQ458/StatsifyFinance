5fa05abf34cd2b8394d85d18805cd8eca6b53ae7
LQ <143152892+LQ458@users.noreply.github.com>
2024-07-09 18:31:31 +0800

    topbar

--- app/(topbar)/page.tsx
+++ app/(topbar)/page.tsx
@@ -1,29 +1,56 @@
 "use client";
 import Link from "next/link";
 import React from "react";
+import { useState } from "react";
 import { FaRegUserCircle } from "react-icons/fa";
 import { IoSearch } from "react-icons/io5";
 import { IoIosArrowDown } from "react-icons/io";
+import styles from "./topbar.module.css";
 
 const Topbar = () => {
+  const [rotate, setRotate] = useState("original");
+
   return (
-    <div className="grid grid-cols-3 justify-items-center w-[100%] bg-topbar-color">
-      <h1 className="text-2xl text-white font-normal">Statsify Finance</h1>
-      <div className="grid grid-cols-3 text-center space-x-5">
-        <div className="bg-white flex justify-center space-x-1 cursor-pointer">
-          <h4 className="font-[400]">Learn</h4>
-          <IoIosArrowDown className="self-center text-[1.6rem]" />
+    <div className="grid grid-cols-3 justify-items-center w-[100%] bg-topbar-color h-[4em]">
+      <h1 className="text-2xl text-white font-normal self-center">
+        Statsify Finance
+      </h1>
+      <div className="grid grid-cols-3 text-center">
+        <div
+          className="relative flex"
+          onMouseEnter={() => setRotate("true")}
+          onMouseLeave={() => setRotate("false")}
+        >
+          <div className="bg-white flex justify-center space-x-1 cursor-pointer relative w-[7em]">
+            <h4 className="font-[400] self-center">Learn</h4>
+            <IoIosArrowDown
+              className={`self-center relative ${rotate === "true" && styles.iconRotate} ${rotate === "false" && styles.iconOriginal} text-[1.6em]`}
+            />
+          </div>
+          {rotate === "true" && (
+            <div className="absolute h-[8rem] w-[7em] bg-white top-[4em] grid-rows-2 left-0 grid p-0">
+              <Link
+                href="#"
+                className="justify-center flex no-underline text-black h-[4em] pr-9"
+              >
+                <h4 className="self-center font-[400]">分析</h4>
+              </Link>
+              <Link href="#" className="justify-center flex no-underline text-black h-[4em] pr-9">
+                <h4 className="self-center font-[400]">策略</h4>
+              </Link>
+            </div>
+          )}
         </div>
-        <Link className="text-white no-underline" href="#">
-          <h4 className="font-[400]">资讯</h4>
+        <Link className="text-white no-underline flex justify-center" href="#">
+          <h4 className="font-[400] self-center">资讯</h4>
         </Link>
-        <Link className="text-white no-underline" href="#">
-          <h4 className="font-[400]">金融基础术语</h4>
+        <Link className="text-white no-underline flex justify-center" href="#">
+          <h4 className="font-[400] self-center">金融基础术语</h4>
         </Link>
       </div>
       <div className="flex space-x-12">
         <Link href="/login" className="flex text-white space-x-3 no-underline">
-          <h4 className="font-[400]">Login</h4>
+          <h4 className="font-[400] self-center">Login</h4>
           <FaRegUserCircle className="text-[1.9rem] self-center" />
         </Link>
         <button
