4ca5567673de1435c42f8064783637001966b31e
Leo Yihao Qin <yihao.qin17311-biph@basischina.com>
2024-11-04 21:40:15 +0800

    Update mongodb.ts

--- libs/mongodb.ts
+++ libs/mongodb.ts
@@ -3,24 +3,16 @@ import mongoose from "mongoose";
 // Connect to MongoDB
 async function DBconnect() {
   try {
-    if (process.env.MONGODB_URL) {
+    if (process.env.MONGODB_URI) {
       if (mongoose.connection.readyState === 1) {
         console.log("Already connected to MongoDB");
         return;
       }
-      await mongoose.connect(process.env.MONGODB_URL);
+      await mongoose.connect(process.env.MONGODB_URI);
       console.log("Connected to MongoDB");
     }
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
-  }
-}
-
-async function DBdisconnect() {
-  try {
-    mongoose.disconnect();
-  } catch (error) {
-    console.error("Error disconnect to MongoDB:", error);
   }
 }
 
