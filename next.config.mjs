b3bc8184e1a550c034df9afa1d171dc772274d7c
Leo Yihao Qin <yihao.qin17311-biph@basischina.com>
2025-01-11 19:59:08 +0800

    fix: config bug

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,28 @@ const withNextIntl = createNextIntlPlugin();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/zh",
+        permanent: true,
+      },
+      {
+        source: "/login",
+        destination: "/zh/login",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default withNextIntl(nextConfig);
